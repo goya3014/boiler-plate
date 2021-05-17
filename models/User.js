ab2f4a5fa4aade08e0c9e1f0197956a085803356
unknown <goya3014@hanmail.net>
2021-05-18 08:27:51 +0900

    첫 커밋

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+//스키마를 모델로 감쌈.
+const User = mongoose.model('User', userSchema)
+
+//모델을 다른 곳에서도 사용가능
+module.exports = { User }
